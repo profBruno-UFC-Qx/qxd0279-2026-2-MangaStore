09869c5d9ca7857f4c3f11bb9e69b6a68b52a272
Bruno Mateus <brunomateus@gmail.com>
2026-09-03 17:28:24 -0300

    Movido o conteúdo do index para dentro do App.vue e criação do compoenente de NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from '@/components/NavBar.vue'
+</script>
 
 <template>
-  <router-view></router-view>
+  <NavBar />
+
+  <main>
+    <div class="album py-5 bg-body-tertiary">
+      <div class="container">
+        <RouterView />
+      </div>
+    </div>
+  </main>
+
+  <footer class="text-body-secondary py-5">
+    <div class="container">
+      <p class="float-end mb-1"><a href="#">Back to top</a></p>
+      <p class="mb-1">
+        Album example is © Bootstrap, but please download and customize it for yourself!
+      </p>
+      <p class="mb-0">
+        New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/introduction"
+          >getting started guide</a
+        >.
+      </p>
+    </div>
+  </footer>
 </template>
