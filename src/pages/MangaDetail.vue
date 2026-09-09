301664e0cd51688351849a32488c0597e244933a
Bruno Mateus <brunomateus@gmail.com>
2026-09-09 09:37:37 -0300

    Adicionando navegação entre mangas

--- src/pages/MangaDetail.vue
+++ src/pages/MangaDetail.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed, onBeforeMount } from 'vue'
-import { useRoute } from 'vue-router'
+import { onBeforeRouteUpdate, useRoute } from 'vue-router'
 import { useUpload } from '@/api'
 import type { Manga } from '@/types'
 import { MangaService } from '@/api/MangaService'
@@ -22,6 +22,20 @@ onBeforeMount(async () => {
   }
 })
 
+onBeforeRouteUpdate(async (to, from) => {
+  if (to.params.id !== from.params.id) {
+    {
+      try {
+        manga.value = await MangaService.findById(to.params.id as string)
+      } catch (e) {
+        error.value = (e as Error).message
+      } finally {
+        loading.value = false
+      }
+    }
+  }
+})
+
 const coverUrl = computed(() => (manga.value ? useUpload(manga.value.cover.url) : ''))
 </script>
 
@@ -38,7 +52,7 @@ const coverUrl = computed(() => (manga.value ? useUpload(manga.value.cover.url) 
           class="rounded-start w-100 h-100 object-fit-cover"
         />
       </div>
-      <div class="col-md-8">
+      <div class="col-md-8 position-relative">
         <div class="card-body">
           <h5 class="card-title">{{ manga.title }}</h5>
           <h6 class="card-subtitle mb-2 text-body-secondary">Volume {{ manga.number }}</h6>
@@ -47,7 +61,22 @@ const coverUrl = computed(() => (manga.value ? useUpload(manga.value.cover.url) 
             Preço: <span class="text-danger">{{ manga.price }}</span>
           </p>
         </div>
+        <nav class="position-absolute bottom-0 end-0 m-2">
+          <button class="btn btn-outline-secondary mx-1">
+            <router-link :to="`/manga/${manga.id - 1}`">Anterior</router-link>
+          </button>
+          <button class="btn btn-outline-secondary">
+            <router-link :to="`/manga/${manga.id + 1}`">Próximo</router-link>
+          </button>
+        </nav>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+nav a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
