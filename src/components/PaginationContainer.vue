1fbd0732d537cffd31964d54c0ca450cb887617c
Bruno Mateus <brunomateus@gmail.com>
2026-09-10 16:10:01 -0300

    Refatoração: Criando um componente de paginação

---
+++ src/components/PaginationContainer.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import type { MetaInformation } from '@/api/MangaService'
+
+const router = useRouter()
+const props = defineProps<MetaInformation>()
+</script>
+
+<template>
+  <nav class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group me-2" role="group" aria-label="First group">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        :disabled="props.pagination.page == 1"
+        @click="router.push('/?page=1')"
+      >
+        Primeira
+      </button>
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        :disabled="props.pagination.page == 1"
+        @click="router.push(`/?page=${props.pagination.page - 1}`)"
+      >
+        Anterior
+      </button>
+      <button
+        v-for="i in props.pagination?.pageCount"
+        :key="i"
+        type="button"
+        class="btn"
+        :class="[i == props.pagination.page ? 'btn-secondary' : 'btn-outline-secondary']"
+        @click="router.push(`/?page=${i}`)"
+      >
+        {{ i }}
+      </button>
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        :disabled="props.pagination.page == props.pagination.pageCount"
+        @click="router.push(`/?page=${props.pagination.page + 1}`)"
+      >
+        Próximo
+      </button>
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        :disabled="props.pagination.page == props.pagination.pageCount"
+        @click="router.push(`/?page=${props.pagination.pageCount}`)"
+      >
+        Última
+      </button>
+    </div>
+  </nav>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from 'vue'
-import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router'
+import { onBeforeRouteUpdate, useRoute } from 'vue-router'
 import { type Manga } from '@/types'
 import { MangaService, type MetaInformation } from '@/api/MangaService'
 import MangaCard from '@/components/MangaCard.vue'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
 import Alert from '@/components/Alert.vue'
+import PaginationContainer from '@/components/PaginationContainer.vue'
 
 const route = useRoute()
-const router = useRouter()
 const mangas = ref<Manga[]>([])
 const loading = ref(true)
 const error = ref<string | null>(null)
 const meta = ref<MetaInformation>({} as MetaInformation)
 
-async function loadMangas(page = 1) {
+async function loadMangas(page: number) {
   try {
-    const result = await MangaService.findAll(page)
+    const result = await MangaService.findAll(page || 1)
     mangas.value = result.data
     meta.value = result.meta
   } catch (e) {
@@ -38,55 +38,10 @@ onBeforeRouteUpdate(async (to, from) => {
   <LoadingSpinner v-if="loading" label="Carregando mangás…" />
   <Alert v-else-if="error" :message="error"></Alert>
   <template v-else>
-    <nav
-      class="btn-toolbar mb-3 d-flex justify-content-center"
-      role="toolbar"
-      aria-label="Toolbar with button groups"
-    >
-      <div class="btn-group me-2" role="group" aria-label="First group">
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          :disabled="meta.pagination.page == 1"
-          @click="router.push('/?page=1')"
-        >
-          Primeira
-        </button>
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          :disabled="meta.pagination.page == 1"
-          @click="router.push(`/?page=${meta.pagination.page - 1}`)"
-        >
-          Anterior
-        </button>
-        <button
-          v-for="i in meta.pagination?.pageCount"
-          :key="i"
-          type="button"
-          class="btn btn-outline-secondary"
-          @click="router.push(`/?page=${i}`)"
-        >
-          {{ i }}
-        </button>
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          :disabled="meta.pagination.page == meta.pagination.pageCount"
-          @click="router.push(`/?page=${meta.pagination.page + 1}`)"
-        >
-          Próximo
-        </button>
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          :disabled="meta.pagination.page == meta.pagination.pageCount"
-          @click="router.push(`/?page=${meta.pagination.pageCount}`)"
-        >
-          Última
-        </button>
-      </div>
-    </nav>
+    <PaginationContainer
+      class="mb-3 d-flex justify-content-center"
+      v-bind="meta"
+    ></PaginationContainer>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       <MangaCard
         v-for="manga of mangas"
@@ -99,5 +54,9 @@ onBeforeRouteUpdate(async (to, from) => {
         :number="manga.number"
       />
     </div>
+    <PaginationContainer
+      class="mt-3 d-flex justify-content-center"
+      v-bind="meta"
+    ></PaginationContainer>
   </template>
 </template>
